15afd913ad9a8001d552d374dc5a22a3a1cc47f8
Deneir Uy <deneiruy@gmail.com>
2024-02-15 14:30:44 +0800

    Add linters & formatters

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local window = vim.wo
 
 opt.scrolloff = 999
+
+window.colorcolumn = "120"
 
 vim.filetype.add({
   extension = {
---
+++ lua/plugins/tailwind.lua
@@ -0,0 +1,15 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tailwindcss = {
+        -- exclude a filetype from the default_config
+        filetypes_exclude = { "markdown" },
+        -- add additional filetypes to the default_config
+        filetypes_include = { "htmldjango", "jinja.html", "django" },
+        -- to fully override the default_config, change the below
+        -- filetypes = {}
+      },
+    },
+  },
+}
